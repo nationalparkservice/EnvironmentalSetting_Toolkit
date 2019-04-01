8a2e2c73509f39a1979414dc1f26e661d5da3a13
Nelson <lnelson>
2019-04-01 16:56:28 -0600

    added getRunDetails(); need to QC and add test

--- ES_Toolkit_R/R/utilityFunctions.R
+++ ES_Toolkit_R/R/utilityFunctions.R
@@ -1428,6 +1428,113 @@ getRunCounts <-
     return(dfResponse)
   }
 
+#' getRunDetails 
+#' 
+#' Formats raw run response into a data frame with year, run lengths, and run end dates.
+#' @importFrom utils write.table
+#' @param rawCounts run response for a climate parameter (use getWxObservations() with a reduce code of 'run*' to generate)
+#' @param runLength number of run days used to filter raw run counts
+#' @param metric (optional) One climate metric from the IMD Environmental Setting protocol
+#' @param filePathAndName (optional) File path and name including extension for output CSV file
+#' @return A data frame of stations and their run year, run lengths, and run end dates
+#' @export getRunDetails
+#' 
+getRunDetails <-
+  function(rawCounts,
+           runLength,
+           metric = NULL,
+           filePathAndName = NULL) {
+    dfResponse0 <- NULL
+    df <- NULL
+    idArray <-  NULL
+    nArray <- NULL
+    dArray <- NULL
+    cArray <- NULL
+    metricArray <- NULL
+    
+    # Get date vector from rawCounts
+    dates <- as.Date(rawCounts$date, "%Y")
+    #countDuration <- rawCounts$date
+    countDuration <- unique(format(dates, "%Y"))
+    yearCount <- length(countDuration)
+    stationCount <- length(unique(rawCounts$uid))
+    rowCount <- yearCount * stationCount
+    
+    idArray <- vector(mode = "integer")
+    nArray <- vector(mode = "character")
+    dArray <- vector(mode = "character")
+    rArray <- vector(mode = "integer")
+    rdArray <- vector(mode = "character")
+    metricArray <- vector(mode = "character")
+    #metricArray <- rep(metric, rowCount)
+    k <- 1
+    
+    for (i in 1:length(unique(rawCounts$uid))) {
+      # Get data for each station for year
+      #print(unique(toCount$uid)[j])
+      byStation <- subset(rawCounts, uid == unique(rawCounts$uid)[i])
+      #byStation0 <- subset(rawCounts, uid == unique(rawCounts$uid)[i])
+      #byStation <- byStation0[with(byStation0, order(date)), ]
+      for (j in 1:length(byStation$uid)) {
+      # Count total greater than or equal to runLength
+      #length(csp3check$pcpn_in_run[14][[1]][,1][as.numeric(csp3check$pcpn_in_run[14][[1]][,1]) >= 7])
+      # Missing/NA element
+      if (is.null(byStation$pcpn_in_run) || is.null(byStation$pcpn_in_run[j][[1]][,1])) {
+        runDetails <- NA
+      }
+      else if (is.na(byStation$pcpn_in_run) || is.na(byStation$pcpn_in_run[j][[1]][,1])) {
+        runDetails <- NA
+      }  
+      # Run element(s) exist
+      else if (length(byStation$pcpn_in_run[j][[1]][, 1]) > 1) {
+        #rD <- subset(byStation, as.numeric(byStation$pcpn_in_run[1][[1]][, 1]) >= runLength])
+        runDays <-
+          byStation$pcpn_in_run[j][[1]][, 1][as.numeric(byStation$pcpn_in_run[j][[1]][, 1]) >= runLength]
+        runEndDates <- 
+          byStation$pcpn_in_run[j][[1]][, 2][as.numeric(byStation$pcpn_in_run[j][[1]][, 1]) >= runLength]
+        #runCounts <-
+        #  as.numeric(byStation$pcpn_in_run[1][[1]][, 1][as.numeric(byStation$pcpn_in_run[1][[1]][, 1]) >= runLength])
+        #runDates <- 
+         # byStation$pcpn_in_run[1][[1]][, 1][as.numeric(byStation$pcpn_in_run[1][[1]][, 2]) >= runLength]
+       # byStationRuns <- cbind(rep(byStation$uid[1], length(runCounts)), rep(byStation$name[1], length(runCounts)), rep(byStation$date[1], length(runCounts)), runCounts, runDates, rep(metric, length(runCounts)))
+        for (k in 1:length(runDays)) {
+          idArray[k] <- byStation$uid[j] 
+          nArray[k] <- byStation$name[j]
+          dArray[k] <- byStation$date[j] 
+          rArray[k] <- runDays[k]
+          rdArray[k] <- runEndDates[k]
+          metricArray[k] <- metric
+        }
+        dfResponse0 <- cbind(idArray, nArray, dArray, as.numeric(rArray), rdArray, metricArray)
+      }
+      df <- rbind(df, dfResponse0)
+      }
+    }
+
+    dfResponse <- as.data.frame(df) #df[order(df[, 1], df[, 3]), ]
+    cntName <- paste("pcpn_in_run_le_0.01",runLength, sep = "_")
+    cntDateName <- paste("pcpn_in_runEndDate",runLength, sep = "_")
+    colnames(dfResponse)[1] <- "uid"
+    colnames(dfResponse)[2] <- "name"
+    colnames(dfResponse)[3] <- "date"
+    colnames(dfResponse)[4] <- cntName
+    colnames(dfResponse)[5] <- cntDateName
+    colnames(dfResponse)[6] <- "metric"
+    
+    # Output file
+    if (!is.null(filePathAndName)) {
+      write.table(
+        dfResponse,
+        file = filePathAndName,
+        sep = ",",
+        row.names = FALSE,
+        qmethod = "double"
+      )
+    }
+    
+    return(dfResponse)
+  }
+
 #' getStationMetrics 
 #' 
 #' Requests Environmental Setting protocol metrics for a set of stations
